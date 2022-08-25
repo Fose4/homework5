f6eecd541992987528243b78393916a73baca8cf
Artem <Styoganov@bk.ru>
2022-08-26 01:05:21 +0300

    commit1

---
+++ task34/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt( int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+
+int EvenNumbers(int[] array)
+{   
+    int quantity = 0;
+    for (int i = 0; i<array.Length; i++ )
+    {
+        if (array[i] % 2 == 0)
+        {
+            quantity++;
+        }
+    }
+    return quantity;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length -1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+int[] arr = CreateArrayRndInt(4, 99,998);
+PrintArray(arr);
+int quantity = EvenNumbers(arr);
+Console.WriteLine($" -->  {quantity}");
---
+++ task38/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] CreateArrayRndInt(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+double[] GetMaxMinElem(double[] array)
+{
+    double minNum =  array [0];
+    double maxNum =  array [0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > maxNum)
+        {
+            maxNum = array[i];
+        }
+        if (array[i] < minNum)
+        {
+            minNum = array[i];
+        }
+        
+    }
+    double diff = maxNum - minNum;
+    return new double[] { diff };
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("] -> ");
+}
+double[] arr = CreateArrayRndInt(4, 0,99);
+PrintArray(arr);
+
+double[] maxMinElem = GetMaxMinElem(arr);
+Console.Write(string.Join(",", maxMinElem));
+//Console.WriteLine($"-> {maxMinElem}");
